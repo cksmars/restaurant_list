6b8ebe0eb7d2ea1f68e51e9660ebbfa710b52cd4
cksmars <cks.mars@gmail.com>
2022-04-25 23:26:56 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const express = require('express')
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 
-// 載入 mongoose
-const mongoose = require('mongoose')
-
 const bodyParser = require('body-parser')
 
 // 載入 method-override
@@ -14,24 +11,10 @@ const methodOverride = require('method-override')
 
 const routes = require('./routes')
 
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
-
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,18 +1,11 @@
-const mongoose = require('mongoose')
-const Restaurant = require('../restaurant') // 載入 Restaurant
+// 載入 Restaurant
+const Restaurant = require('../restaurant')
+
 const restaurantList = require('../../restaurant.json')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-
   Restaurant.create(restaurantList.results)
   console.log('done')
 })
